755f59e08eff75055c6de2e7a41653d12eaf1fb8
John Mills <jpm23456@gmail.com>
2022-07-01 10:23:47 -0500

    removed deploy from git

--- deploy/bundle/main.js
+++
@@ -1,9 +0,0 @@
-chrome.action.onClicked.addListener(function(tab) {
-  chrome.tabs.create({
-    url: chrome.runtime.getURL("index.html")
-  },() => {
-    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-      console.log(request)
-    });
-  });
-});
